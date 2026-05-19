b6a97bf13d5835944d992b27a7e733647107da28
Mirjeta Mustafa <mustafamirjeta11@gmail.com>
2026-05-19 16:19:58 +0200

    fix: update button focus outline styles

--- frontend/src/components/ui/Button.tsx
+++ frontend/src/components/ui/Button.tsx
@@ -11,18 +11,15 @@ const Button = ({
   size?: 'small' | 'medium' | 'large'
 }) => {
   const baseClasses =
-    'focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2'
+    'focus:outline-none focus:ring-2 focus:ring-[#D4A853] focus:ring-offset-2 rounded transition-colors cursor-pointer duration-200'
   const variantClasses = clsx({
-    'bg-[#D4A853] text-white cursor-pointer ease-in-out duration-200 hover:bg-[#C09A4A]':
-      variant === 'primary',
-    'bg-white text-gray-800 border border-gray-300 cursor-pointer ease-in-out duration-200 hover:bg-black hover:bg-black hover:text-white':
+    'bg-[#D4A853] text-white hover:bg-[#C09A4A]': variant === 'primary',
+    'bg-white text-gray-800 border border-gray-300 hover:bg-black hover:bg-black hover:text-white':
       variant === 'secondary',
-    'bg-black text-white cursor-pointer ease-in-out duration-200 hover:bg-[#D4A853]':
-      variant === 'default',
-    'bg-transparent border border-gray-300 text-gray-800 cursor-pointer ease-in-out duration-200 hover:bg-[#E0E0E0]':
+    'bg-black text-white  hover:bg-[#D4A853]': variant === 'default',
+    'bg-transparent border border-gray-300 text-gray-800  hover:bg-[#E0E0E0]':
       variant === 'blur',
-    'bg-white text-gray-800 cursor-pointer ease-in-out duration-200 hover:bg-gray-100':
-      variant === 'ghost',
+    'bg-white text-gray-800  hover:bg-gray-100': variant === 'ghost',
   })
   const sizeClasses = clsx({
     'px-4 py-2 text-sm': size === 'small',
