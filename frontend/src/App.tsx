a9711547312884ef7285fc7a3a48af27526e8f74
Mirjeta Mustafa <mustafamirjeta11@gmail.com>
2026-05-19 16:06:54 +0200

    feat: create reusable Button component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,24 @@
+import Button from './components/ui/Button'
+
 const App = () => {
   return (
-    <div>
-      <h1 className="text-3xl text-orange-700">Hello, Vite + React!</h1>
+    <div className="flex gap-5 p-5">
+      <Button variant="primary" size="medium">
+        Primary Button
+      </Button>
+      <Button variant="secondary" size="medium">
+        Secondary Button
+      </Button>
+      <Button variant="default" size="large">
+        Default Button
+      </Button>
+
+      <Button variant="blur" size="medium">
+        Blur Button
+      </Button>
+      <Button variant="ghost" size="small">
+        Gho
+      </Button>
     </div>
   )
 }
---
+++ frontend/src/components/ui/Button.tsx
@@ -0,0 +1,41 @@
+// Button component with variants and sizes
+import React from 'react'
+import clsx from 'clsx'
+
+const Button = ({
+  variant = 'default',
+  size = 'medium',
+  ...props
+}: React.ButtonHTMLAttributes<HTMLButtonElement> & {
+  variant?: 'default' | 'primary' | 'secondary' | 'blur' | 'ghost'
+  size?: 'small' | 'medium' | 'large'
+}) => {
+  const baseClasses =
+    'focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2'
+  const variantClasses = clsx({
+    'bg-[#D4A853] text-white cursor-pointer ease-in-out duration-200 hover:bg-[#C09A4A]':
+      variant === 'primary',
+    'bg-white text-gray-800 border border-gray-300 cursor-pointer ease-in-out duration-200 hover:bg-black hover:bg-black hover:text-white':
+      variant === 'secondary',
+    'bg-black text-white cursor-pointer ease-in-out duration-200 hover:bg-[#D4A853]':
+      variant === 'default',
+    'bg-transparent border border-gray-300 text-gray-800 cursor-pointer ease-in-out duration-200 hover:bg-[#E0E0E0]':
+      variant === 'blur',
+    'bg-white text-gray-800 cursor-pointer ease-in-out duration-200 hover:bg-gray-100':
+      variant === 'ghost',
+  })
+  const sizeClasses = clsx({
+    'px-4 py-2 text-sm': size === 'small',
+    'px-6 py-3 text-base': size === 'medium',
+    'px-8 py-4 text-lg': size === 'large',
+  })
+
+  return (
+    <button
+      className={clsx(baseClasses, variantClasses, sizeClasses)}
+      {...props}
+    />
+  )
+}
+
+export default Button
