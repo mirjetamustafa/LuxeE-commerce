22d0d23d6c7da4acff34044e8a34f7dbf18ad765
Mirjeta Mustafa <mustafamirjeta11@gmail.com>
2026-06-01 14:51:57 +0200

    feat: add responsive navbar with mobile sidebar

--- frontend/src/components/layout/Navbar.tsx
+++ frontend/src/components/layout/Navbar.tsx
@@ -1,48 +1,96 @@
 // Navbar component for the e-commerce website with tailwind css styling with logo, home, shop, about, contact, search, cart, user, and favorite icons and resonsive design for mobile devices
 
-import { Search, ShoppingCart, User, Heart } from 'lucide-react'
-import { Link } from 'react-router-dom'
+import { Search, ShoppingCart, User, Heart, Menu } from 'lucide-react'
+import React from 'react'
+import { NavLink, Link } from 'react-router-dom'
+import Sidebar from './Sidebar'
 
 const Navbar = () => {
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = React.useState(false)
+
+  const toggleMobileMenu = () => {
+    setIsMobileMenuOpen(!isMobileMenuOpen)
+  }
   return (
     <nav className="bg-white shadow-md">
-      <div className="container mx-auto px-4 py-2 flex items-center justify-around">
+      <div className="container mx-auto px-4 py-2 flex items-center justify-between md:justify-around">
+        <div className="text-gray-800 hover:text-[#D4A853] cursor-pointer duration-200 mt-2 md:hidden block">
+          <button onClick={toggleMobileMenu} className="cursor-pointer">
+            <Menu className="w-6 h-6" />
+          </button>
+        </div>
         <div className="text-2xl font-bold text-gray-800 font-playfair uppercase">
           <Link to="/">
             Luxe<span className="text-[#D4A853]">.</span>
           </Link>
         </div>
         <div className="hidden md:flex space-x-6">
-          <Link
+          <NavLink
             to="/"
-            className="text-gray-600 font-medium hover:text-[#D4A853] duration-300 "
+            className={({ isActive }) =>
+              `font-medium hover:text-[#D4A853] duration-300 ${
+                isActive ? 'text-[#D4A853]' : 'text-gray-600'
+              }`
+            }
           >
             Home
-          </Link>
-          <Link
+          </NavLink>
+          <NavLink
             to="/shop"
-            className="text-gray-600 font-medium hover:text-[#D4A853] duration-300 "
+            className={({ isActive }) =>
+              `font-medium hover:text-[#D4A853] duration-300 ${
+                isActive ? 'text-[#D4A853]' : 'text-gray-600'
+              }`
+            }
           >
             Shop
-          </Link>
-          <Link
+          </NavLink>
+          <NavLink
             to="/about"
-            className="text-gray-600 font-medium hover:text-[#D4A853] duration-300 "
+            className={({ isActive }) =>
+              `font-medium hover:text-[#D4A853] duration-300 ${
+                isActive ? 'text-[#D4A853]' : 'text-gray-600'
+              }`
+            }
           >
             About
-          </Link>
-          <Link
+          </NavLink>
+          <NavLink
             to="/contact"
-            className="text-gray-600 font-medium hover:text-[#D4A853] duration-300 "
+            className={({ isActive }) =>
+              `font-medium hover:text-[#D4A853] duration-300 ${
+                isActive ? 'text-[#D4A853]' : 'text-gray-600'
+              }`
+            }
           >
             Contact
-          </Link>
+          </NavLink>
         </div>
-        <div className="flex items-center space-x-4">
-          <Search className="text-gray-600 hover:text-[#D4A853] cursor-pointer duration-300 " />
-          <ShoppingCart className="text-gray-600 hover:text-[#D4A853] cursor-pointer duration-300 " />
+        <div className="flex items-center space-x-9">
+          <Search className="text-gray-600 hidden md:flex hover:text-[#D4A853] cursor-pointer duration-300 " />
           <User className="text-gray-600 hover:text-[#D4A853] cursor-pointer duration-300 " />
-          <Heart className="text-gray-600 hover:text-[#D4A853] cursor-pointer duration-300 " />
+          <Heart className="text-gray-600 hidden md:flex hover:text-[#D4A853] cursor-pointer duration-300 " />
+          <ShoppingCart className="text-gray-600 hover:text-[#D4A853] cursor-pointer duration-300 " />
+        </div>
+      </div>
+
+      {/* Mobile Menu */}
+
+      <div
+        className={`fixed inset-0 z-50 md:hidden transition-opacity duration-300 ${
+          isMobileMenuOpen ? 'opacity-100 visible' : 'opacity-0 invisible'
+        }`}
+      >
+        <div
+          className="absolute inset-0 bg-black/50"
+          onClick={toggleMobileMenu}
+        />
+        <div
+          className={`absolute top-0 left-0 h-screen w-64 bg-white shadow-lg transition-transform duration-300 ${
+            isMobileMenuOpen ? 'translate-x-0' : '-translate-x-full'
+          }`}
+        >
+          <Sidebar toggleMobileMenu={toggleMobileMenu} />
         </div>
       </div>
     </nav>
---
+++ frontend/src/components/layout/Sidebar.tsx
@@ -0,0 +1,97 @@
+// sidebar component for mobile view
+
+import { Heart, Search, User, X } from 'lucide-react'
+import { NavLink, Link } from 'react-router-dom'
+import Input from '../ui/Input'
+const Sidebar = ({ toggleMobileMenu }: { toggleMobileMenu: () => void }) => {
+  return (
+    <div className="bg-white pt-4 flex flex-col h-screen">
+      <div className="flex justify-between items-center px-4 pb-5">
+        <div className="text-xl font-bold text-gray-800 font-playfair uppercase">
+          <Link to="/">
+            Luxe<span className="text-[#D4A853]">.</span>
+          </Link>
+        </div>
+        <button
+          className="text-gray-600 hover:text-[#D4A853] cursor-pointer duration-300"
+          onClick={toggleMobileMenu}
+        >
+          <X className="w-6 h-6" />
+        </button>
+      </div>
+      <div className="border-y border-gray-200 p-4">
+        <Input
+          type="search"
+          placeholder="Search products..."
+          leftIcon={<Search className="w-5 h-5" />}
+          inputSize="sm"
+        />
+      </div>
+      <nav className="flex flex-col space-y-2 mt-4 px-2">
+        <NavLink
+          to="/"
+          className={({ isActive }) =>
+            `font-medium rounded-md p-4 duration-300 ${
+              isActive
+                ? 'text-[#D4A853] bg-[#D4A853]/10'
+                : 'text-gray-600 hover:bg-gray-100'
+            }`
+          }
+        >
+          Home
+        </NavLink>
+        <NavLink
+          to="/shop"
+          className={({ isActive }) =>
+            `font-medium rounded-md p-4 duration-300 ${
+              isActive
+                ? 'text-[#D4A853] bg-[#D4A853]/10'
+                : 'text-gray-600 hover:bg-gray-100'
+            }`
+          }
+        >
+          Shop
+        </NavLink>
+        <NavLink
+          to="/about"
+          className={({ isActive }) =>
+            `font-medium rounded-md p-4 duration-300 ${
+              isActive
+                ? 'text-[#D4A853] bg-[#D4A853]/10'
+                : 'text-gray-600 hover:bg-gray-100'
+            }`
+          }
+        >
+          About
+        </NavLink>
+        <NavLink
+          to="/contact"
+          className={({ isActive }) =>
+            `font-medium rounded-md p-4 duration-300 ${
+              isActive
+                ? 'text-[#D4A853] bg-[#D4A853]/10'
+                : 'text-gray-600 hover:bg-gray-100'
+            }`
+          }
+        >
+          Contact
+        </NavLink>
+      </nav>
+
+      <div className="mt-auto border-t  border-gray-200 bg-gray-50 p-4">
+        <div className="flex justify-around">
+          <p className="flex flex-col items-center font-semibold text-gray-600 hover:text-[#D4A853] cursor-pointer ">
+            <User className="w-5 h-5" />
+            <span className="text-xs">Account</span>
+          </p>
+          <p className="flex flex-col items-center font-semibold text-gray-600 hover:text-[#D4A853] cursor-pointer">
+            <Heart className="w-5 h-5" />
+            <span className="text-xs">Wishlist</span>
+          </p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Sidebar
