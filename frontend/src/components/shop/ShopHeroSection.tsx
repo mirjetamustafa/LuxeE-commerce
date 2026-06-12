edaaec2ece43ddb9d50c32146fd7009bb071d937
Mirjeta Mustafa <mustafamirjeta11@gmail.com>
2026-06-12 17:19:55 +0200

    feat(shop): add Shop hero section

---
+++ frontend/src/components/shop/ShopHeroSection.tsx
@@ -0,0 +1,17 @@
+const ShopHeroSection = () => {
+  return (
+    <div className="bg-[#F9F9F9] border-b border-[#E5E7EB] py-12 mb-5">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 text-center">
+        <h2 className="font-bold font-playfair text-3xl md:text-4xl mb-4">
+          All Products
+        </h2>
+        <p className="text-gray-800 max-w-xl mx-auto ">
+          Discover our complete collection of thoughtfully designed essentials
+          for your everyday life.
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default ShopHeroSection
--- frontend/src/pages/Shop.tsx
+++ frontend/src/pages/Shop.tsx
@@ -1,46 +1,9 @@
+import ShopHeroSection from '../components/shop/ShopHeroSection'
+
 const Shop = () => {
   return (
     <div className="mt-20">
-      <h1 className="text-3xl font-bold">shop</h1>
-      <h1 className="text-3xl font-bold">shop</h1>
-      <h1 className="text-3xl font-bold">shop</h1>
-      <h1 className="text-3xl font-bold">shop</h1>
-      <h1 className="text-3xl font-bold">shop</h1>
-      <h1 className="text-3xl font-bold">shop</h1>
-      <h1 className="text-3xl font-bold">shop</h1>
-      <h1 className="text-3xl font-bold">shop</h1>
-      <h1 className="text-3xl font-bold">shop</h1>
-      <h1 className="text-3xl font-bold">shop</h1>
-      <h1 className="text-3xl font-bold">shop</h1>
-      <h1 className="text-3xl font-bold">shop</h1>
-      <h1 className="text-3xl font-bold">shop</h1>
-      <h1 className="text-3xl font-bold">shop</h1>
-      <h1 className="text-3xl font-bold">shop</h1>
-      <h1 className="text-3xl font-bold">shop</h1>
-      <h1 className="text-3xl font-bold">shop</h1>
-      <h1 className="text-3xl font-bold">shop</h1>
-      <h1 className="text-3xl font-bold">shop</h1>
-      <h1 className="text-3xl font-bold">shop</h1>
-      <h1 className="text-3xl font-bold">shop</h1>
-      <h1 className="text-3xl font-bold">shop</h1>
-      <h1 className="text-3xl font-bold">shop</h1>
-      <h1 className="text-3xl font-bold">shop</h1>
-      <h1 className="text-3xl font-bold">shop</h1>
-      <h1 className="text-3xl font-bold">shop</h1>
-      <h1 className="text-3xl font-bold">shop</h1>
-      <h1 className="text-3xl font-bold">shop</h1>
-      <h1 className="text-3xl font-bold">shop</h1>
-      <h1 className="text-3xl font-bold">shop</h1>
-      <h1 className="text-3xl font-bold">shop</h1>
-      <h1 className="text-3xl font-bold">shop</h1>
-      <h1 className="text-3xl font-bold">shop</h1>
-      <h1 className="text-3xl font-bold">shop</h1>
-      <h1 className="text-3xl font-bold">shop</h1>
-      <h1 className="text-3xl font-bold">shop</h1>
-      <h1 className="text-3xl font-bold">shop</h1>
-      <h1 className="text-3xl font-bold">shop</h1>
-      <h1 className="text-3xl font-bold">shop</h1>
-      <h1 className="text-3xl font-bold">shop</h1>
+      <ShopHeroSection />
     </div>
   )
 }
