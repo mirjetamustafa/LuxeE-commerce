35a2636ecc6794376581c69850a992bf582783cb
Mirjeta Mustafa <mustafamirjeta11@gmail.com>
2026-06-12 12:54:27 +0200

    update button

--- frontend/src/components/ui/Button.tsx
+++ frontend/src/components/ui/Button.tsx
@@ -5,13 +5,15 @@ import clsx from 'clsx'
 const Button = ({
   variant = 'default',
   size = 'medium',
+  fullWidth = false,
   ...props
 }: React.ButtonHTMLAttributes<HTMLButtonElement> & {
   variant?: 'default' | 'primary' | 'secondary' | 'blur' | 'ghost'
   size?: 'small' | 'medium' | 'large'
+  fullWidth?: boolean
 }) => {
   const baseClasses =
-    'focus:outline-none focus:ring-2 focus:ring-[#D4A853] focus:ring-offset-2 transition-colors cursor-pointer duration-200'
+    'inline-flex items-center justify-center focus:outline-none focus:ring-2 focus:ring-[#D4A853] focus:ring-offset-2 transition-colors cursor-pointer duration-200'
   const variantClasses = clsx({
     'bg-[#D4A853] text-white hover:bg-[#C09A4A]': variant === 'primary',
     'bg-[#F9F9F9] text-gray-900 border border-gray-500 hover:bg-black hover:bg-black hover:text-white':
@@ -24,12 +26,17 @@ const Button = ({
   const sizeClasses = clsx({
     'px-4 py-2 text-sm': size === 'small',
     'px-6 py-3 text-base font-semibold': size === 'medium',
-    'px-6 py-3 w-full text-xl': size === 'large',
+    'px-6 py-3 text-lg font-medium': size === 'large',
   })
 
   return (
     <button
-      className={clsx(baseClasses, variantClasses, sizeClasses)}
+      className={clsx(
+        baseClasses,
+        variantClasses,
+        sizeClasses,
+        fullWidth && 'w-full',
+      )}
       {...props}
     />
   )
