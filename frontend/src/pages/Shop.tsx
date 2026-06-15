af47ce21ca8f0ff317bfee577cabc6e4d5954448
Mirjeta Mustafa <mustafamirjeta11@gmail.com>
2026-06-15 17:28:23 +0200

    feat(shop): add products at shop page

--- frontend/src/pages/Shop.tsx
+++ frontend/src/pages/Shop.tsx
@@ -1,15 +1,18 @@
 import Filters from '../components/shop/Filters'
+import Products from '../components/shop/Products'
 import ShopHeroSection from '../components/shop/ShopHeroSection'
 
 const Shop = () => {
   return (
-    <div className="mt-20">
+    <div className="mt-20 mx-5 md:mx-0">
       <ShopHeroSection />
-      <div className="flex items-center max-w-7xl mx-auto gap-5">
-        <div className="w-100 my-9">
+      <div className="flex  max-w-7xl mx-auto gap-7">
+        <div className="w-100 my-8 hidden md:block">
           <Filters />
         </div>
-        <div className="w-full shrink bg-red-200">02</div>
+        <div className="w-full my-6">
+          <Products />
+        </div>
       </div>
     </div>
   )
