61eb0a680ffc8eded31a114bdbebd7908fd21a2d
Mirjeta Mustafa <mustafamirjeta11@gmail.com>
2026-06-17 13:00:10 +0200

    feat: add reusable axios API utility

---
+++ frontend/src/api/Api.tsx
@@ -0,0 +1,41 @@
+import axios, { AxiosError, type AxiosRequestConfig } from 'axios'
+
+export interface AxiosRequestOptions<
+  D = unknown,
+> extends AxiosRequestConfig<D> {
+  url: string
+}
+
+export async function apiRequest<D = unknown, R = unknown>({
+  url,
+  method = 'GET',
+  data,
+  params,
+  headers = {},
+}: AxiosRequestOptions<D>): Promise<R> {
+  try {
+    const token = localStorage.getItem('token')
+
+    const response = await axios.request<R>({
+      baseURL: import.meta.env.VITE_SERVER_URL,
+      url,
+      method,
+      data,
+      params,
+      headers: {
+        'Content-Type': 'application/json',
+        ...headers,
+        ...(token && {
+          Authorization: `Bearer ${token}`,
+        }),
+      },
+    })
+
+    return response.data
+  } catch (error) {
+    const err = error as AxiosError
+
+    console.error('API request faled:', err.response?.data || err.message)
+    throw err.response?.data || err
+  }
+}
