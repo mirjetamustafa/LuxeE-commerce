34989b767e08d9e67d6da15bb38c47907ae4b9c1
Mirjeta Mustafa <mustafamirjeta11@gmail.com>
2026-06-18 15:39:14 +0200

    feat: implement functional user login form

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,39 +1,46 @@
-import { Link, NavLink } from 'react-router-dom'
+import { Link, NavLink, useNavigate } from 'react-router-dom'
 import Input from '../components/ui/Input'
 import Button from '../components/ui/Button'
+import type { LoginInput } from '../api/User/user.types'
+import { useState } from 'react'
 import { loginUser } from '../api/User/user'
-import { useEffect } from 'react'
+import { toast } from 'react-toastify'
 
-// interface UserLoginData {
-//   email: string
-//   password: string
-// }
+const initialFormLogin: LoginInput = {
+  email: '',
+  password: '',
+}
 
 const Login = () => {
-  // const [formData, setFormData] = useState({
-  //   email: '',
-  //   password: '',
-  // })
+  const [formData, setFormData] = useState(initialFormLogin)
+  const navigate = useNavigate()
 
-  // const handleSubmit = async (e: React.FormEvent) => {
-  //   e.preventDefault()
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setFormData((prev) => ({
+      ...prev,
+      [event.target.name]: event.target.value,
+    }))
+  }
 
-  //   try {
-  //     const res = await loginUser(formData)
-  //     console.log('Login success:', res)
-  //   } catch (error) {
-  //     console.error(error)
-  //   }
-  // }
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault()
 
-  useEffect(() => {
-    loginUser({
-      email: 'mustafamirjeta11@gmail.com',
-      password: '1234567889',
-    })
-      .then(console.log)
-      .catch(console.error)
-  }, [])
+    if (!formData.email || !formData.password) {
+      toast.error('Please fill all fields')
+      return
+    }
+
+    try {
+      const response = await loginUser(formData)
+      localStorage.setItem('token', `Bearer ${response.token}`)
+      console.log('User Loged in:', response)
+      toast.success('Login Successfully!')
+      navigate('/user')
+    } catch (error) {
+      console.error(error)
+      toast.error(error?.response?.data?.message || 'Login failed')
+    }
+  }
 
   return (
     <div className="flex items-center justify-center min-h-screen bg-[#F9F9F9]">
@@ -66,23 +73,29 @@ const Login = () => {
         <h2 className="text-2xl font-playfair font-bold text-[#1a1a1a] mb-6 text-center">
           Welcome Back
         </h2>
-        <form action="" className="space-y-4">
+        <form onSubmit={handleSubmit} className="space-y-4">
           <Input
+            name="email"
             variant="login"
             inputSize="lg"
             type="email"
             label="Email Address"
+            value={formData.email}
+            onChange={handleChange}
           />
           <Input
+            name="password"
             variant="login"
             inputSize="lg"
             type="password"
             label="Password"
+            value={formData.password}
+            onChange={handleChange}
           />
           <div className="text-right text-xs cursor-pointer text-[#1a1a1a] hover:text-[#D4A853]">
             <Link to="">Forgot password?</Link>
           </div>
-          <Button variant="default" size="medium" fullWidth>
+          <Button type="submit" variant="default" size="medium" fullWidth>
             Sign In
           </Button>
         </form>
