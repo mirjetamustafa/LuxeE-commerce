1d7f5c803d445412133efb50c2eaf9248df66049
Mirjeta Mustafa <mustafamirjeta11@gmail.com>
2026-06-22 16:17:57 +0200

    fix(admin): limit scrolling to main content area

--- frontend/src/pages/admin/AdminLayout.tsx
+++ frontend/src/pages/admin/AdminLayout.tsx
@@ -3,10 +3,10 @@ import Sidebar from '../../components/admin/Sidebar'
 
 const AdminLayout = () => {
   return (
-    <div className="flex min-h-screen bg-[#F9F9F9]">
+    <div className="flex h-screen overflow-hidden bg-[#F9F9F9]">
       <Sidebar />
 
-      <div className="flex-1 bg-gray-100 p-6">
+      <div className="flex-1 overflow-y-auto  bg-gray-100 p-6">
         <Outlet />
       </div>
     </div>
