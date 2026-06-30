9c32e7fa626c2651955009bc4881d12ee3305903
Mirjeta Mustafa <mustafamirjeta11@gmail.com>
2026-06-30 16:23:20 +0200

    feat(admin): fetch products from database at admin dashboard

--- frontend/src/pages/admin/Products.tsx
+++ frontend/src/pages/admin/Products.tsx
@@ -1,19 +1,36 @@
 import { Edit, Search, Trash } from 'lucide-react'
 import Button from '../../components/ui/Button'
-import Product28 from '../../assets/products/product28.jfif'
-import { useState } from 'react'
+
 import Input from '../../components/ui/Input'
 import Select from '../../components/ui/Select'
+import { useProducts } from '../../hooks/useProducts'
+import { useNavigate } from 'react-router-dom'
 
 const Products = () => {
-  const [active, setActive] = useState('Active')
+  const { products, loading, handlDelete } = useProducts()
+
+  const navigate = useNavigate()
+
+  if (loading) {
+    return <p className="p-5">Loading products...</p>
+  }
+
   return (
     <div className="mt-5">
-      <div className="mb-9">
-        <h3 className="text-2xl font-bold ">Products</h3>
-        <p className="text-gray-500 mt-1">
-          Manage your inventory and product catalog.
-        </p>
+      <div className="mb-9 flex flex-col md:flex-row justify-between">
+        <div>
+          <h3 className="text-2xl font-bold ">Products</h3>
+          <p className="text-gray-500 mt-1">
+            Manage your inventory and product catalog.
+          </p>
+        </div>
+        <Button
+          variant="next"
+          size="small"
+          onClick={() => navigate('/admin/add-product')}
+        >
+          Add Product
+        </Button>
       </div>
 
       <div className="flex items-center gap-2 bg-white rounded-xl p-5 mb-9">
@@ -65,7 +82,67 @@ const Products = () => {
           </thead>
 
           <tbody className="divide-y divide-gray-100">
-            <tr className="bg-white group hover:bg-gray-50">
+            {products.map((prod) => (
+              <tr key={prod._id} className="bg-white group hover:bg-gray-50">
+                <td className="px-6 py-4  last:rounded-b-3xl">
+                  <div className="flex items-center gap-3 ">
+                    <div className="w-10 h-10 flex items-center justify-center">
+                      <img
+                        src={`http://localhost:5000/uploads/${prod.image}`}
+                        alt=""
+                        className="rounded-sm"
+                      />
+                    </div>
+                    <div className="flex flex-col">
+                      <span className="font-medium text-gray-900">
+                        {prod.title}
+                      </span>
+                      <span className="text-xs text-gray-500 uppercase">
+                        {prod.sku}
+                      </span>
+                    </div>
+                  </div>
+                </td>
+                <td className="px-6 py-4 text-gray-600">
+                  {prod.category?.name}
+                </td>
+                <td className="px-6 py-4 text-gray-900">${prod.price}</td>
+
+                <td className="px-6 py-4">{prod.stock} in stock</td>
+                <td className="px-6 py-4">
+                  {' '}
+                  {prod.status === 'active' ? (
+                    <span className="px-3 py-1 rounded-full text-xs font-medium bg-green-100 text-green-700">
+                      Active
+                    </span>
+                  ) : (
+                    <span className="px-3 py-1 rounded-full text-xs font-medium bg-gray-200 text-gray-600">
+                      Draft
+                    </span>
+                  )}
+                </td>
+                <td className="px-6 py-4">
+                  <div className="flex gap-3 text-gray-900 opacity-0 group-hover:opacity-100 transition">
+                    <Button
+                      variant="edit"
+                      onClick={() =>
+                        navigate(`/admin/edit-product/${prod._id}`)
+                      }
+                    >
+                      <Edit size={15} />
+                    </Button>
+                    <Button
+                      variant="delete"
+                      onClick={() => handlDelete(prod._id)}
+                    >
+                      <Trash size={15} />
+                    </Button>
+                  </div>
+                </td>
+              </tr>
+            ))}
+
+            {/* <tr className="bg-white group hover:bg-gray-50">
               <td className="px-6 py-4  last:rounded-b-3xl">
                 <div className="flex items-center gap-3 ">
                   <div className="w-10 h-10 flex items-center justify-center">
@@ -86,7 +163,7 @@ const Products = () => {
 
               <td className="px-6 py-4">145 in stock</td>
               <td className="px-6 py-4">
-                {!active ? (
+                {active ? (
                   <span className="px-3 py-1 rounded-full text-xs font-medium bg-green-100 text-green-700">
                     Active
                   </span>
@@ -149,50 +226,7 @@ const Products = () => {
                   </Button>
                 </div>
               </td>
-            </tr>
-
-            <tr className="bg-white group hover:bg-gray-50">
-              <td className="px-6 py-4  last:rounded-b-3xl">
-                <div className="flex items-center gap-3 ">
-                  <div className="w-10 h-10 flex items-center justify-center">
-                    <img src={Product28} alt="" className="rounded-sm" />
-                  </div>
-                  <div className="flex flex-col">
-                    <span className="font-medium text-gray-900">
-                      Classic White T-Shirt
-                    </span>
-                    <span className="text-xs text-gray-500 uppercase">
-                      app-tsh-wht-m
-                    </span>
-                  </div>
-                </div>
-              </td>
-              <td className="px-6 py-4 text-gray-600">Apparel</td>
-              <td className="px-6 py-4 text-gray-900">$24.99</td>
-
-              <td className="px-6 py-4">145 in stock</td>
-              <td className="px-6 py-4">
-                {active ? (
-                  <span className="px-3 py-1 rounded-full text-xs font-medium bg-green-100 text-green-700">
-                    Active
-                  </span>
-                ) : (
-                  <span className="px-3 py-1 rounded-full text-xs font-medium bg-gray-200 text-gray-600">
-                    Draft
-                  </span>
-                )}
-              </td>
-              <td className="px-6 py-4">
-                <div className="flex gap-3 text-gray-900 opacity-0 group-hover:opacity-100 transition">
-                  <Button variant="edit">
-                    <Edit size={15} />
-                  </Button>
-                  <Button variant="delete">
-                    <Trash size={15} />
-                  </Button>
-                </div>
-              </td>
-            </tr>
+            </tr> */}
           </tbody>
         </table>
         <div className="flex justify-between px-5 py-3 border border-gray-200">
