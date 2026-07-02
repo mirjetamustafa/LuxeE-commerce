b07cbf8ca20114924567a9540682f8561a1df080
Mirjeta Mustafa <mustafamirjeta11@gmail.com>
2026-07-02 14:04:53 +0200

    feat(contct): add hero section at contact

---
+++ frontend/src/components/contact/GetInTouch.tsx
@@ -0,0 +1,17 @@
+const GetInTouch = () => {
+  return (
+    <div className="bg-[#F9F9F9] border-b border-[#E5E7EB] py-12 mb-5">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 text-center">
+        <h2 className="font-bold font-playfair text-4xl md:text-5xl mb-4">
+          Get in Touch
+        </h2>
+        <p className="text-gray-800 max-w-2xl text-lg mx-auto ">
+          Have a question about our products, your order, or just want to say
+          hello? We'd love to hear from you.
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default GetInTouch
