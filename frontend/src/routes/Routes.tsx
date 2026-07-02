5a9b483b3eebaa924e4cfb965f2a6b0ec6d8a943
Mirjeta Mustafa <mustafamirjeta11@gmail.com>
2026-07-02 16:50:51 +0200

    refactor: reorganize user routes

--- frontend/src/routes/Routes.tsx
+++ frontend/src/routes/Routes.tsx
@@ -1,21 +1,31 @@
+import type { RouteObject } from 'react-router-dom'
+
 import Footer from '../components/layout/Footer'
 import Header from '../components/layout/Header'
 import Navbar from '../components/layout/Navbar'
+
 import About from '../pages/About'
-import AddProduct from '../pages/admin/AddProduct'
-import AdminLayout from '../pages/admin/AdminLayout'
-import Dashboard from '../pages/admin/Dashboard'
-import EditProduct from '../pages/admin/EditProduct'
-import Products from '../pages/admin/Products'
-import Cart from '../pages/Cart'
-import Checkout from '../pages/Checkout'
 import Contact from '../pages/Contact'
 import Home from '../pages/Home'
 import Login from '../pages/Login'
 import ProductDetails from '../pages/ProductDetails'
 import Register from '../pages/Register'
 import Shop from '../pages/Shop'
-import User from '../pages/User'
+import Cart from '../pages/Cart'
+import Checkout from '../pages/Checkout'
+
+import User from '../pages/user/User'
+import ProfileSettings from '../pages/user/ProfileSettings'
+import OrderHistory from '../pages/user/OrderHistory'
+import SavedAddress from '../pages/user/SavedAddress'
+import Wishlist from '../pages/user/Wishlist'
+
+import AdminLayout from '../pages/admin/AdminLayout'
+import Dashboard from '../pages/admin/Dashboard'
+import Products from '../pages/admin/Products'
+import AddProduct from '../pages/admin/AddProduct'
+import EditProduct from '../pages/admin/EditProduct'
+
 import AuthenticationRoute from './AuthenticationRoute'
 
 export const RouteTypes = {
@@ -27,6 +37,7 @@ export const RouteTypes = {
 export type RouteType = (typeof RouteTypes)[keyof typeof RouteTypes]
 
 export const createRoutes = (): RouteObject[] => [
+  // PUBLIC ROUTES
   {
     element: <AuthenticationRoute routeType={RouteTypes.PUBLIC} />,
     children: [
@@ -103,6 +114,7 @@ export const createRoutes = (): RouteObject[] => [
     ],
   },
 
+  // PRIVATE ROUTES
   {
     element: <AuthenticationRoute routeType={RouteTypes.PRIVATE} />,
     children: [
@@ -135,10 +147,29 @@ export const createRoutes = (): RouteObject[] => [
             <Footer />
           </>
         ),
+        children: [
+          {
+            index: true,
+            element: <ProfileSettings />,
+          },
+          {
+            path: 'order-history',
+            element: <OrderHistory />,
+          },
+          {
+            path: 'saved-address',
+            element: <SavedAddress />,
+          },
+          {
+            path: 'wishlist',
+            element: <Wishlist />,
+          },
+        ],
       },
     ],
   },
 
+  // ADMIN ROUTES
   {
     element: <AuthenticationRoute routeType={RouteTypes.ADMIN} />,
     children: [
