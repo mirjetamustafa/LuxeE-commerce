9f2e62b978e69b26f7ca9d6bbc86710e1b2fb324
Mirjeta Mustafa <mustafamirjeta11@gmail.com>
2026-07-06 15:44:53 +0200

    style: update user styles

--- frontend/src/pages/user/User.tsx
+++ frontend/src/pages/user/User.tsx
@@ -4,7 +4,7 @@ import UserSidebar from '../../components/user/UserSidebar'
 const User = () => {
   return (
     <div className="bg-[#F9F9F9]">
-      <div className="flex max-w-5xl mx-auto  mt-20">
+      <div className="flex flex-col md:flex-row max-w-6xl mx-auto gap-8 mt-20">
         <UserSidebar />
         <div className="flex-1">
           <Outlet />
