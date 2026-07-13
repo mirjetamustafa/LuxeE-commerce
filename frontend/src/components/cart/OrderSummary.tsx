06a52a0cf15df10f738d06863e8aea941a3268ea
Mirjeta Mustafa <mustafamirjeta11@gmail.com>
2026-07-13 18:03:40 +0200

    feat(cart): make OrderSummary dynamic with cart data

--- frontend/src/components/cart/OrderSummary.tsx
+++ frontend/src/components/cart/OrderSummary.tsx
@@ -1,7 +1,52 @@
+import { useSelector } from 'react-redux'
+import type { RootState } from '../../redux/store'
+import { setDiscount } from '../../redux/slices/cartSlice'
 import Button from '../ui/Button'
 import Input from '../ui/Input'
+import { useAppDispatch } from '../../redux/hooks'
+import { useState } from 'react'
+import useCartSammuray from '../../hooks/useCartSammuray'
 
 const OrderSummary = () => {
+  const { subtotal, discountAmount, tax, total } = useCartSammuray()
+
+  const [message, setMessage] = useState('')
+
+  const dispatch = useAppDispatch()
+  const { discountCode, discount } = useSelector(
+    (state: RootState) => state.cart,
+  )
+
+  const handleApplyDiscount = () => {
+    const code = discountCode.toLocaleUpperCase()
+
+    if (code === 'WELCOME10') {
+      dispatch(
+        setDiscount({
+          code,
+          discount: 0.1,
+        }),
+      )
+      setMessage(`Code ${discountCode} applied`)
+    } else if (code === 'LUXE20') {
+      dispatch(
+        setDiscount({
+          code,
+          discount: 0.2,
+        }),
+      )
+      setMessage(`Code ${discountCode} applied`)
+    } else {
+      dispatch(
+        setDiscount({
+          code: '',
+          discount: 0,
+        }),
+      )
+      setMessage('Invalid discount code')
+    }
+  }
+
   return (
     <div className="p-6">
       <h2 className="text-lg md:text-xl font-bold font-playfair mb-4">
@@ -14,13 +59,28 @@ const OrderSummary = () => {
             placeholder="Enter code"
             variant="login"
             inputSize="sm"
+            value={discountCode}
+            onChange={(e) =>
+              dispatch(
+                setDiscount({
+                  code: e.target.value,
+                  discount,
+                }),
+              )
+            }
           />
-          <span className="text-xs text-gray-600">
-            Try: WELCOME10 or LUXE20
-          </span>
+          <div className="flex flex-col">
+            {discount > 0 && (
+              <span className="text-xs text-green-600 mt-1"> {message} </span>
+            )}
+
+            <span className="text-xs text-gray-600 mt-1">
+              Try: WELCOME10 or LUXE20
+            </span>
+          </div>
         </div>
         <div className="">
-          <Button variant="default" size="small">
+          <Button variant="default" size="small" onClick={handleApplyDiscount}>
             Apply
           </Button>
         </div>
@@ -29,8 +89,16 @@ const OrderSummary = () => {
       <div className="border-b border-gray-200 pt-3 pb-6">
         <div className="flex justify-between pt-4">
           <p className="text-sm">Subtotal</p>
-          <p className="text-sm font-medium">$338.00</p>
+          <p className="text-sm font-medium">${subtotal.toFixed(2)}</p>
         </div>
+        {discount > 0 && (
+          <div className="flex justify-between pt-4 font-medium text-green-600">
+            <p className="text-sm">Discount ({discountCode})</p>
+            <p className="text-sm font-medium ">
+              -${discountAmount.toFixed(2)}
+            </p>
+          </div>
+        )}
 
         <div className="flex justify-between pt-4">
           <p className="text-sm">Shipping</p>
@@ -39,12 +107,12 @@ const OrderSummary = () => {
 
         <div className="flex justify-between pt-4">
           <p className="text-sm">Estimated Tax</p>
-          <p className="text-sm font-medium">$27.04</p>
+          <p className="text-sm font-medium">${tax.toFixed(2)}</p>
         </div>
       </div>
       <div className="flex justify-between py-4">
         <p className="text-xl font-bold font-playfair">Total</p>
-        <p className="text-xl font-bold font-playfair">$365.04</p>
+        <p className="text-xl font-bold font-playfair">${total.toFixed(2)}</p>
       </div>
       <Button variant="primary" size="large" fullWidth>
         Proceed to Checkout
