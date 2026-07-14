2158a0346bc97d48cf7e901e8eb107ea0bbd283f
Mirjeta Mustafa <mustafamirjeta11@gmail.com>
2026-07-14 17:49:23 +0200

    feat: implement checkout flow

---
+++ frontend/src/pages/Checkout.tsx
@@ -0,0 +1,82 @@
+import { useState } from 'react'
+import ShippingForm from '../components/checkout/ShippingForm'
+import { Check } from 'lucide-react'
+import PaymentMethod from '../components/checkout/PaymentMethod'
+import ReviewOrder from '../components/checkout/ReviewOrder'
+
+const Checkout = () => {
+  const [step, setStep] = useState(1)
+  return (
+    <div className="bg-[#F9F9F9]  pt-24 pb-20">
+      <div className="w-ful md:max-w-6xl mx-9 md:mx-auto py-8">
+        <h1 className="text-xl md:text-3xl font-bold font-playfair mb-8">
+          Checkout
+        </h1>
+
+        <div className="flex justify-center items-center gap-2">
+          <div className="flex justify-center items-center gap-2">
+            <div className="">
+              <div
+                className={`w-10 h-10 flex items-center justify-center font-medium rounded-full ${step >= 1 ? 'bg-[#D4A853] text-white' : 'bg-gray-200 text-gray-400'}`}
+              >
+                {step > 1 ? <Check className="w-4 h-4" /> : <span>1</span>}
+              </div>
+              <span
+                className={`text-xs font-medium ${step >= 1 ? 'text-gray-900' : 'text-gray-400'}`}
+              >
+                Shipping
+              </span>
+            </div>
+            <div
+              className={`w-20 h-0.5 flex mb-5 ${step >= 2 ? 'bg-[#D4A853]' : 'bg-gray-200'}`}
+            ></div>
+          </div>
+
+          <div className="flex justify-center items-center gap-2">
+            <div className="">
+              <div
+                className={`w-10 h-10 flex items-center justify-center font-medium rounded-full ${step >= 2 ? 'bg-[#D4A853] text-white' : 'bg-gray-200 text-gray-400'}`}
+              >
+                {step > 2 ? <Check className="w-4 h-4" /> : <span>2</span>}
+              </div>
+              <span
+                className={`text-xs font-medium ${step >= 2 ? 'text-gray-900' : 'text-gray-400'}`}
+              >
+                Payment
+              </span>
+            </div>
+            <div
+              className={`w-20 h-0.5 flex mb-5 ${step >= 3 ? 'bg-[#D4A853]' : 'bg-gray-200'}`}
+            ></div>
+          </div>
+
+          <div className="flex justify-center items-center gap-2">
+            <div className="">
+              <div
+                className={`w-10 h-10 flex items-center justify-center font-medium rounded-full ${step >= 3 ? 'bg-[#D4A853] text-white' : 'bg-gray-200 text-gray-400'}`}
+              >
+                3
+              </div>
+              <span
+                className={`text-xs font-medium ${step >= 3 ? 'text-gray-900' : 'text-gray-400'}`}
+              >
+                Review
+              </span>
+            </div>
+          </div>
+        </div>
+
+        <div className="flex flex-col md:flex-row gap-9">
+          <div className="flex-1 bg-white my-5">
+            {step === 1 && <ShippingForm setStep={setStep} />}
+            {step === 2 && <PaymentMethod setStep={setStep} />}
+            {step === 3 && <ReviewOrder setStep={setStep} />}
+          </div>
+          <div className="w-full md:w-[350px] h-50 bg-white my-0 md:my-5"></div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Checkout
