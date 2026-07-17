ebc21492cc48c873f0a349a76eef57f7381d8a92
Mirjeta Mustafa <mustafamirjeta11@gmail.com>
2026-07-17 12:27:50 +0200

    feat: add getOrders hook for fetching order

--- frontend/src/hooks/useOrder.ts
+++ frontend/src/hooks/useOrder.ts
@@ -1,6 +1,6 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import type { CreateOrderInput, Order } from '../api/order/order.types'
-import { createOrder } from '../api/order/order'
+import { createOrder, getMyOrders } from '../api/order/order'
 import { useDispatch } from 'react-redux'
 import type { AppDispatch } from '../redux/store'
 import { clearUserCart } from '../redux/slices/cartSlice'
@@ -11,6 +11,7 @@ export const useOrder = () => {
   const [loading, setLoading] = useState(false)
   const [order, setOrder] = useState<Order | null>(null)
   const [error, setError] = useState<string | null>(null)
+  const [getOrders, setGetOrders] = useState<Order[]>([])
 
   const placeOrder = async (data: CreateOrderInput) => {
     try {
@@ -33,8 +34,20 @@ export const useOrder = () => {
     }
   }
 
+  const fetchOrder = async () => {
+    try {
+      const response = await getMyOrders()
+      setGetOrders(response.orders)
+    } catch (error) {
+      console.error(error)
+    }
+  }
+  useEffect(() => {
+    fetchOrder()
+  }, [])
   return {
     placeOrder,
+    getOrders,
     loading,
     order,
     error,
