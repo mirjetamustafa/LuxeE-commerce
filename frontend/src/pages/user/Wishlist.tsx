5ca743abdf6c197b18ff953305eed0ccb825d053
Mirjeta Mustafa <mustafamirjeta11@gmail.com>
2026-07-17 16:27:35 +0200

    feat(wishlist): display wishlist products from redux state

--- frontend/src/pages/user/Wishlist.tsx
+++ frontend/src/pages/user/Wishlist.tsx
@@ -1,13 +1,35 @@
 import { Link } from 'react-router-dom'
-import product1 from '../../assets/products/product1.jfif'
-import product2 from '../../assets/products/product3.jfif'
-import product3 from '../../assets/products/product5.jfif'
-import product4 from '../../assets/products/product17.jfif'
-import product5 from '../../assets/products/product9.jfif'
-import product6 from '../../assets/products/product13.jfif'
+
 import { X } from 'lucide-react'
+import { useDispatch, useSelector } from 'react-redux'
+import type { AppDispatch, RootState } from '../../redux/store'
+import { useEffect } from 'react'
+import {
+  fetchWishlist,
+  removeProductToWishlist,
+} from '../../redux/slices/wishlistSlice'
+import { toast } from 'react-toastify'
 
 const Wishlist = () => {
+  const dispatch = useDispatch<AppDispatch>()
+
+  const { items } = useSelector((state: RootState) => state.wishlist)
+
+  useEffect(() => {
+    dispatch(fetchWishlist())
+  }, [dispatch])
+
+  const handleDelete = (
+    e: React.MouseEvent<HTMLButtonElement>,
+    productId: string,
+  ) => {
+    e.preventDefault()
+    e.stopPropagation()
+
+    dispatch(removeProductToWishlist(productId))
+    toast.success('Product deleted succesfully')
+  }
+
   return (
     <div className="bg-white p-6 shadow-xs my-20">
       <h2 className="text-xl md:text-2xl font-bold font-playfair mb-4">
@@ -15,154 +37,40 @@ const Wishlist = () => {
       </h2>
 
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-2">
-        <div className="group overflow-hidden">
-          <Link to={'/'} className="block overflow-hidden">
-            <div className="relative h-60 w-full overflow-hidden">
-              {/* Foto kryesore */}
-              <img
-                src={product1}
-                alt=""
-                className="w-full h-full object-cover transition duration-300 group-hover:scale-105"
-              />
-              <div className="flex flex-col absolute top-0 right-0 m-2">
-                <button className="bg-white text-gray-700 rounded-full p-1 cursor-pointer hover:text-red-600">
-                  <X className="w-4 h-4" />
-                </button>
+        {items.map((product) => (
+          <div key={product._id} className="group overflow-hidden">
+            <Link
+              to={`/products/${product._id}`}
+              className="block overflow-hidden"
+            >
+              <div className="relative h-60 w-full overflow-hidden">
+                {/* Foto kryesore */}
+                <img
+                  src={`http://localhost:5000${product.image}`}
+                  alt={product.title}
+                  className="w-full h-full object-cover transition duration-300 group-hover:scale-105"
+                />
+                <div className="flex flex-col absolute top-0 right-0 m-2">
+                  <button
+                    onClick={(e) => handleDelete(e, product._id)}
+                    className="bg-white text-gray-700 rounded-full p-1 cursor-pointer hover:text-red-600"
+                  >
+                    <X className="w-4 h-4" />
+                  </button>
+                </div>
               </div>
-            </div>
-          </Link>
+            </Link>
 
-          <div className="p-4">
-            <div className="text-sm font-medium text-gray-900 hover:text-[#D4A853]">
-              <Link to="">Minimalist Wireless Headphones</Link>
-            </div>
-            <p className="text-sm font-medium text-gray-900">$249.99</p>
-          </div>
-        </div>
-
-        <div className="group overflow-hidden">
-          <Link to={'/'} className="block overflow-hidden">
-            <div className="relative h-60 w-full overflow-hidden">
-              {/* Foto kryesore */}
-              <img
-                src={product2}
-                alt=""
-                className="w-full h-full object-cover transition duration-300 group-hover:scale-105"
-              />
-
-              <div className="flex flex-col absolute top-0 right-0 m-2">
-                <button className="bg-white text-gray-700 rounded-full p-1 cursor-pointer hover:text-red-600">
-                  <X className="w-4 h-4" />
-                </button>
+            <div className="p-4">
+              <div className="text-sm font-medium text-gray-900 hover:text-[#D4A853]">
+                <Link to={`/products/${product._id}`}> {product.title} </Link>
               </div>
+              <p className="text-sm font-medium text-gray-900">
+                ${product.price.toFixed(2)}
+              </p>
             </div>
-          </Link>
-
-          <div className="p-4">
-            <div className="text-sm font-medium text-gray-900 hover:text-[#D4A853]">
-              <Link to="">Minimalist Wireless Headphones</Link>
-            </div>
-            <p className="text-sm font-medium text-gray-900">$249.99</p>
           </div>
-        </div>
-
-        <div className="group overflow-hidden">
-          <Link to={'/'} className="block overflow-hidden">
-            <div className="relative h-60 w-full overflow-hidden">
-              {/* Foto kryesore */}
-              <img
-                src={product3}
-                alt=""
-                className="w-full h-full object-cover transition duration-300 group-hover:scale-105"
-              />
-              <div className="flex flex-col absolute top-0 right-0 m-2">
-                <button className="bg-white text-gray-700 rounded-full p-1 cursor-pointer hover:text-red-600">
-                  <X className="w-4 h-4" />
-                </button>
-              </div>
-            </div>
-          </Link>
-
-          <div className="p-4">
-            <div className="text-sm font-medium text-gray-900 hover:text-[#D4A853]">
-              <Link to="">Minimalist Wireless Headphones</Link>
-            </div>
-            <p className="text-sm font-medium text-gray-900">$249.99</p>
-          </div>
-        </div>
-
-        <div className="group overflow-hidden">
-          <Link to={'/'} className="block overflow-hidden">
-            <div className="relative h-60 w-full overflow-hidden">
-              {/* Foto kryesore */}
-              <img
-                src={product4}
-                alt=""
-                className="w-full h-full object-cover transition duration-300 group-hover:scale-105"
-              />
-              <div className="flex flex-col absolute top-0 right-0 m-2">
-                <button className="bg-white text-gray-700 rounded-full p-1 cursor-pointer hover:text-red-600">
-                  <X className="w-4 h-4" />
-                </button>
-              </div>
-            </div>
-          </Link>
-
-          <div className="p-4">
-            <div className="text-sm font-medium text-gray-900 hover:text-[#D4A853]">
-              <Link to="">Minimalist Wireless Headphones</Link>
-            </div>
-            <p className="text-sm font-medium text-gray-900">$249.99</p>
-          </div>
-        </div>
-        <div className="group overflow-hidden">
-          <Link to={'/'} className="block overflow-hidden">
-            <div className="relative h-60 w-full overflow-hidden">
-              {/* Foto kryesore */}
-              <img
-                src={product5}
-                alt=""
-                className="w-full h-full object-cover transition duration-300 group-hover:scale-105"
-              />
-              <div className="flex flex-col absolute top-0 right-0 m-2">
-                <button className="bg-white text-gray-700 rounded-full p-1 cursor-pointer hover:text-red-600">
-                  <X className="w-4 h-4" />
-                </button>
-              </div>
-            </div>
-          </Link>
-
-          <div className="p-4">
-            <div className="text-sm font-medium text-gray-900 hover:text-[#D4A853]">
-              <Link to="">Minimalist Wireless Headphones</Link>
-            </div>
-            <p className="text-sm font-medium text-gray-900">$249.99</p>
-          </div>
-        </div>
-        <div className="group overflow-hidden">
-          <Link to={'/'} className="block overflow-hidden">
-            <div className="relative h-60 w-full overflow-hidden">
-              {/* Foto kryesore */}
-              <img
-                src={product6}
-                alt=""
-                className="w-full h-full object-cover transition duration-300 group-hover:scale-105"
-              />
-              <div className="flex flex-col absolute top-0 right-0 m-2">
-                <button className="bg-white text-gray-700 rounded-full p-1 cursor-pointer hover:text-red-600">
-                  <X className="w-4 h-4" />
-                </button>
-              </div>
-            </div>
-          </Link>
-
-          <div className="p-4">
-            <div className="text-sm font-medium text-gray-900 hover:text-[#D4A853]">
-              <Link to="">Minimalist Wireless Headphones</Link>
-            </div>
-            <p className="text-sm font-medium text-gray-900">$249.99</p>
-          </div>
-        </div>
+        ))}
       </div>
     </div>
   )
