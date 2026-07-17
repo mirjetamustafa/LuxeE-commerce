51dfba0f8b1a302a5b5ef27e491885c6fd606ab5
Mirjeta Mustafa <mustafamirjeta11@gmail.com>
2026-07-17 16:28:53 +0200

    feat(redux): add order and wishlist reducers to store

--- frontend/src/redux/store.ts
+++ frontend/src/redux/store.ts
@@ -1,9 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit'
 import cartReducer from './slices/cartSlice'
+import orderReducer from './slices/orderSlice'
+import wishlistReducer from './slices/wishlistSlice'
 
 export const store = configureStore({
   reducer: {
     cart: cartReducer,
+    order: orderReducer,
+    wishlist: wishlistReducer,
   },
 })
 
