a4586f9f87463be146df808ff24044fb576ed474
Mirjeta Mustafa <mustafamirjeta11@gmail.com>
2026-07-27 13:52:38 +0200

    feat(shop): connect product filters with shop components

--- frontend/src/pages/Shop.tsx
+++ frontend/src/pages/Shop.tsx
@@ -1,17 +1,35 @@
 import Filters from '../components/shop/Filters'
 import Products from '../components/shop/Products'
 import ShopHeroSection from '../components/shop/ShopHeroSection'
+import useProductFilters from '../hooks/useProductFilters'
 
 const Shop = () => {
+  const {
+    products,
+    filters,
+    updateCategories,
+    updatePrice,
+    updateRating,
+    updateStock,
+    updateSearch,
+    clearFilters,
+  } = useProductFilters()
   return (
     <div className="mt-20 mx-5 md:mx-0">
       <ShopHeroSection />
       <div className="flex  max-w-7xl mx-auto gap-7">
         <div className="w-100 my-8 hidden md:block">
-          <Filters />
+          <Filters
+            filters={filters}
+            updateCategories={updateCategories}
+            updatePrice={updatePrice}
+            updateRating={updateRating}
+            updateStock={updateStock}
+            clearFilters={clearFilters}
+          />
         </div>
         <div className="w-full my-6">
-          <Products />
+          <Products products={products} updateSearch={updateSearch} />
         </div>
       </div>
     </div>
