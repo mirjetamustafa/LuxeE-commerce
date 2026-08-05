04bcf28cf3746db671d10f4cd005d26264699079
Mirjeta Mustafa <mustafamirjeta11@gmail.com>
2026-08-05 17:24:09 +0200

    feat: display payment method and status in order details

--- frontend/src/components/admin/productOrderDetails/Payment.tsx
+++ frontend/src/components/admin/productOrderDetails/Payment.tsx
@@ -1,6 +1,31 @@
 import { CreditCard } from 'lucide-react'
+import type { Order } from '../../../api/order/order.types'
 
-const Payment = () => {
+interface PaymentProps {
+  order: Order
+}
+
+const Payment = ({ order }: PaymentProps) => {
+  const getPaymentStatus = () => {
+    if (order.status === 'Cancelled') {
+      return 'Refunded'
+    }
+
+    if (order.paymentMethod === 'Cash on Delivery') {
+      if (order.status === 'Pending') {
+        return 'Pending Payment'
+      }
+
+      return 'Paid'
+    }
+
+    if (order.status === 'Pending') {
+      return 'Pending'
+    }
+
+    return 'Paid'
+  }
+
   return (
     <div className="border border-slate-200 bg-white rounded-xl shadow-xs py-5 px-6 mt-6">
       <div className="flex items-center gap-2">
@@ -8,8 +33,21 @@ const Payment = () => {
         <div className="font-semibold">Payment</div>
       </div>
       <div className="text-sm text-slate-500 space-y-1 mt-3">
-        <p className="font-medium">Visa ending in 4242</p>
-        <p className="text-green-600 text-sm">Paid</p>
+        {order.paymentMethod === 'Credit Card' ? (
+          <p className="font-medium">Visa ending in 4242</p>
+        ) : (
+          <p className="font-medium">Cash on Delivery</p>
+        )}
+
+        <p
+          className={
+            order.status === 'Cancelled'
+              ? 'text-red-500 text-sm'
+              : 'text-green-600 text-sm'
+          }
+        >
+          {getPaymentStatus()}
+        </p>
       </div>
     </div>
   )
