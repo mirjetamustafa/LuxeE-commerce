c4d4f9bc51a8bc23eec8a362ac30524b7a86fafa
Mirjeta Mustafa <mustafamirjeta11@gmail.com>
2026-08-05 17:23:12 +0200

    feat: display customer details in admin order view

--- frontend/src/components/admin/productOrderDetails/Customer.tsx
+++ frontend/src/components/admin/productOrderDetails/Customer.tsx
@@ -1,16 +1,32 @@
 import { User } from 'lucide-react'
+import type { Order } from '../../../api/order/order.types'
 
-const Customer = () => {
+interface CustomerProps {
+  order: Order
+}
+
+const Customer = ({ order }: CustomerProps) => {
   return (
     <div className="border border-slate-200 bg-white rounded-xl shadow-xs py-5 px-6">
       <div className="flex items-center gap-2">
         <User className="w-5 h-5 text-indigo-700" />
         <div className="font-semibold">Customer</div>
       </div>
-      <p className="font-semibold mt-5">Maya Thompson</p>
-      <p className="text-sm text-indigo-700 mt-1">maya.thompson@example.com</p>
+      <p className="font-semibold mt-5">
+        {order.shippingAddress.firstName} {order.shippingAddress.lastName}
+      </p>
+      <p className="text-sm text-indigo-700 mt-1">
+        {order.shippingAddress.email}
+      </p>
 
-      <div className="text-sm text-slate-500 mt-4">Customer since May 2025</div>
+      <div className="text-sm text-slate-500 mt-4">
+        Customer since{' '}
+        {order.customer.createdAt &&
+          new Date(order.customer.createdAt).toLocaleDateString('en-US', {
+            month: 'long',
+            year: 'numeric',
+          })}
+      </div>
     </div>
   )
 }
