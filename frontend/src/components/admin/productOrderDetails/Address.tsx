9c3f9e1794b720d9a90f8acd56c73301b72a58a9
Mirjeta Mustafa <mustafamirjeta11@gmail.com>
2026-08-05 17:22:15 +0200

    feat: display order shipping address details in admin panel

--- frontend/src/components/admin/productOrderDetails/Address.tsx
+++ frontend/src/components/admin/productOrderDetails/Address.tsx
@@ -1,6 +1,11 @@
 import { MapPin } from 'lucide-react'
+import type { Order } from '../../../api/order/order.types'
 
-const Address = () => {
+interface AddressProps {
+  order: Order
+}
+
+const Address = ({ order }: AddressProps) => {
   return (
     <div className="border border-slate-200 bg-white rounded-xl shadow-xs py-5 px-6 mt-6">
       <div className="flex items-center gap-2">
@@ -8,10 +13,14 @@ const Address = () => {
         <div className="font-semibold">Shipping Address</div>
       </div>
       <div className="text-sm text-slate-500 space-y-1 mt-3">
-        <p className="">Maya Thompson</p>
-        <p className="">142 Willoughby Street</p>
-        <p className="">Brooklyn, NY 11201</p>
-        <p className="">United States</p>
+        <p className="">
+          {order.shippingAddress.firstName} {order.shippingAddress.lastName}
+        </p>
+        <p className="">{order.shippingAddress.streetAddress}</p>
+        <p className="">
+          {order.shippingAddress.city}, {order.shippingAddress.zipCode}
+        </p>
+        <p className="">{order.shippingAddress.state}</p>
       </div>
     </div>
   )
